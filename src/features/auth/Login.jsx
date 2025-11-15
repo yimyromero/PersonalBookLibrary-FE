f7b89ec7ec8f716b3f62f0d11be92012750de6ab
Yimy.Romero <yimy.romero80@gmail.com>
2025-11-14 23:55:28 -0500

    (update) Adjust login page. Should be responsive and button disabled if both fields are not provided.

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -3,7 +3,7 @@ import { useNavigate, Link  } from 'react-router';
 import { useDispatch } from 'react-redux';
 import { setCredentials } from './authSlice';
 import { useLoginMutation } from './authApiSlice';
-import { DocumentPlusIcon } from '@heroicons/react/24/solid';
+import { ArrowLeftIcon } from '@heroicons/react/24/solid';
 
 const Login = () => {
     const userRef = useRef();
@@ -51,16 +51,16 @@ const Login = () => {
             //errRef.current.focus();
         }
     }
-    const canSave = true;
+    const canSave = [username, password].every(Boolean);
     
     const content = (
-        <section className="flex justify-center items-center flex-col h-screen">
+        <section className="flex justify-center items-center flex-col h-screen overflow-scroll py-10">
             <header><h1 className="text-2xl text-center font-bold text-slate-800">Sign in</h1></header>
-            <main className="flex items-center justify-center flex-col w-90 max-w-xl mx-auto">
+            <main className="flex items-center justify-center flex-col w-full max-w-md mx-auto">
                 <div className="absolute bg-yellow-100">
                     <p ref={errRef} className="text-red-500" aria-live="assertive">{errMsg}</p>
                 </div>
-                <form className="flex flex-col p-10 bg-white w-full" onSubmit={handleSubmit}>
+                <form className="flex flex-col px-3 pt-7 bg-white w-full" onSubmit={handleSubmit}>
                     <label className="text-gray-600 text-sm font-bold pb-1" htmlFor="username">Username:</label>
                     <input
                         className="rounded border border-gray-300 bg-gray-100 mb-4 px-2 py-1 focus:outline-none"
@@ -71,6 +71,7 @@ const Login = () => {
                         onChange={handleUserInput}
                         autoComplete="off"
                         required
+                        placeholder="username"
                     />
                     <label className="text-gray-600 text-sm font-bold pb-1" htmlFor="password">Password:</label>
                     <input
@@ -81,6 +82,7 @@ const Login = () => {
                         onChange={handlePwdInput}
                         autoComplete="off"
                         required
+                        placeholder="password"
                     />
                     <div className="mt-4">
                         <button className="flex w-full justify-center gap-2 items-center text-white py-2 px-4 bg-red-400 hover:bg-red-500 disabled:bg-gray-300 disabled:cursor-not-allowed cursor-pointer rounded-full" title="Save" disabled={!canSave}>
@@ -91,14 +93,14 @@ const Login = () => {
                         <p className="text-center text-gray-600">Don't have an account?</p>
                     </div>
                     <div className="mt-4">
-                        <button className="flex w-full text-gray-600 justify-center gap-2 items-center border-gray-600 border-2 py-2 px-4 bg-white hover:bg-red-500 disabled:bg-gray-300 disabled:cursor-not-allowed cursor-pointer rounded-full" title="Save" disabled={!canSave}>
+                        <button className="flex w-full text-gray-800 justify-center gap-2 items-center outline-gray-600 outline-2 -outline-offset-2 py-2 px-4 bg-white hover:bg-gray-600 hover:text-white disabled:bg-gray-300 disabled:cursor-not-allowed cursor-pointer rounded-full" title="Create account">
                                 Create Account
                         </button>
                     </div>
                 </form>
             </main>
-            <footer>
-                <Link to="/">Back to Home</Link>
+            <footer className="flex items-center gap-2 mt-10 text-gray-600 hover:text-gray-800">
+                <ArrowLeftIcon className="size-5" /> <Link to="/">Back to Home</Link>
             </footer>
         </section>
     )
