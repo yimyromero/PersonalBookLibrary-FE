ab01cdd897ee609f8e315e8faf28084f3b2b861b
Yimy.Romero <yimy.romero80@gmail.com>
2025-11-18 23:34:04 -0500

    (feat) Update public facing page, with copy and buttons to the sign-in and sign up routes.

--- src/components/Public.jsx
+++ src/components/Public.jsx
@@ -1,25 +1,42 @@
 import { Link } from "react-router";
+import { useState, useRef } from "react";
+import { BookOpenIcon } from "@heroicons/react/24/outline";
 
 const Public = () => {
-  const content = (
-    <div className="bg-neutral-50 text-neutral-900">
-      <h1 className="text-red-800 font-bold text-3xl mb-4">Library Catalog</h1>
-      <p className="text-neutral-700 mb-6">
-        Discover, borrow, and enjoy your favorite books.
-      </p>
+	const content = (
+		<div className="bg-rose-200 bg-linear-to-br to-rose-100 from-gray-100 from-67% h-screen">
+			<div className="container mx-auto">
+				<div className="flex justify-center items-center flex-col pt-30">
+					<BookOpenIcon className="size-14 text-red-400" />
+					<h1 className="text-4xl/14 text-gray-700 font-bold text-center ">
+						Organize Your Personal Library,
+						<br />
+						Simple & Easy
+					</h1>
+					<p className="pt-10 text-gray-400">
+						Keep track of your favorite books, and find recommendations
+					</p>
+					<div className="pt-16">
+						<a
+							href="/login"
+							className="flex w-full justify-center gap-2 items-center text-white py-2 px-4 bg-red-400 hover:bg-red-500 disabled:bg-gray-300 disabled:cursor-not-allowed cursor-pointer rounded-full">
+							Sign In
+						</a>
+						<div className="mt-7">
+							<span className="text-gray-600">Or </span>
+							<Link
+								to={{ pathname: "/signup" }}
+								className="text-gray-700 font-bold hover:underline">
+								Sign Up
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 
-      <button className="bg-red-600 hover:bg-red-700 text-white font-semibold px-4 py-2 rounded">
-        Borrow Book
-      </button>
-
-      <ul className="divide-y divide-neutral-200 mt-6">
-        <li className="hover:bg-red-50 px-3 py-2 cursor-pointer">Book Title 1</li>
-        <li className="hover:bg-red-50 px-3 py-2 cursor-pointer">Book Title 2</li>
-      </ul>
-    </div>
-  );
-
-  return content;
+	return content;
 };
 
 export default Public;
