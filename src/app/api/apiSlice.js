9af9ee738d212ac1dd00ec3ce44e9d13b8698baa
Yimy.Romero <yimy.romero80@gmail.com>
2025-11-20 23:51:09 -0500

    (feat) Add authorization header for api end points. If the token expires it will refresh the token.

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,33 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { setCredentials, logOut } from "../../features/auth/authSlice";
+
+const baseQuery = fetchBaseQuery({
+	baseUrl: "http://localhost:3500",
+	credentials: "include",
+	prepareHeaders: (headers, { getState }) => {
+		const token = getState().auth.token;
+		if (token) {
+			headers.set("authorization", `Bearer ${token}`);
+		}
+		return headers;
+	},
+});
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+	let result = await baseQuery(args, api, extraOptions);
+	if (result.error && result.error.status === 403) {
+		const refreshResult = await baseQuery("/auth/refresh", api, extraOptions);
+		if (refreshResult?.data) {
+			api.dispatch(setCredentials(refreshResult.data));
+			result = await baseQuery(args, api, extraOptions);
+		} else {
+			api.dispatch(logOut());
+		}
+	}
+	return result;
+};
 
 export const apiSlice = createApi({
-	baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
+	baseQuery: baseQueryWithReauth,
 	tagTypes: ["Book", "User", "Borrows"],
 	endpoints: (builder) => ({}),
 });
