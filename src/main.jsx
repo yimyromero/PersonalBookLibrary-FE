7adbffd55d577ff46684b97a977a778e95f13c01
Yimy.Romero <yimy.romero80@gmail.com>
2025-12-15 23:14:16 -0500

    (deploy) Changes to deploy.

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import { store } from './app/store.js';
 import { Provider } from 'react-redux';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
